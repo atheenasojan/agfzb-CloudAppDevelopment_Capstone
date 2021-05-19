0f50b27711d2df0e4546070135c8a37603038d48
atheenasojan <atheenasojan246@gmail.com>
2021-05-19 20:23:14 +0000

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -22,6 +22,9 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('registration/', views.registration_request, name='registration'),
 
     # path for dealer reviews view
 
